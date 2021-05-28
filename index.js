f18262c743d1da5e7dcdb51f24e1d21dc6c677cf
julianxlozano <julianxlozano@gmail.com>
2021-05-28 15:09:07 -0500

    commit tree a9aae41eddb5df018e9b6052680eb1969106295e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(username,useremail){
+
+    let dataObj = {
+        "name":"Steve",
+        "email":"steve@steve.com"
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify(dataObj)
+    };
+    return fetch('http://localhost:3000/users',configObj).then(function(response) {
+        return response.json();
+      }).then(function(object) {
+        let body = document.getElementsByTagName('body')[0]
+        body.innerHTML = object.id 
+      }).catch(function(error){
+        let body = document.getElementsByTagName('body')[0]
+        body.innerHTML = error.message 
+      })
+};
